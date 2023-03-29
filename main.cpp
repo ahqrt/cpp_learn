1b06c9eab4d70afb37358882e3a5cdae7dfaa182
yangliye <1217223113@qq.com>
2023-03-29 18:26:33 +0800

    Revert "feat: const在类型前和类型后的区别"
    This reverts commit 824bc9ad92161fad902e9649bde0181a8181e8d4.

--- main.cpp
+++ main.cpp
@@ -1,32 +1,15 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <array>
 
 using namespace std;
 int sum_arr(int arr[], int n);
-void plusArrElement(vector<int>* arr);
 
 int main() {
     const int ArSize = 8;
     int cookies[ArSize] {1, 2, 4, 8, 16, 32, 64, 128};
-//    array<int, ArSize> cookies = {1, 2, 4, 8, 16, 32, 64, 128};
-
-//    const 在类型前 和 类型后
-//    const在类型前，代表不能修改指向的值，但是可以修改指针的指向
-//    const int* pt  = cookies;
-//    pt = new int;
-
-//    const在类型后，代表不能修改指针的指向，但是可以修改指向的值
-//    int* const pt  = cookies;
-//    pt[0] += 1;
-
-//    vector<int> cookies {1, 2, 4, 8, 16, 32, 64, 128};
-//    plusArrElement(&cookies);
-    // int sum = sum_arr(cookies, ArSize);
-    for (int i = 0; i< ArSize; i++) {
-        cout << cookies[i] << " ";
-    }
+    int sum = sum_arr(cookies, ArSize);
+    cout << sum << endl;
     return 0;
 }
 
@@ -37,13 +20,6 @@ int sum_arr(int arr[], int n)
         total += arr[i];
     }
     return total;
-}
-
-void plusArrElement(vector<int>* arr)
-{
-    for (int i = 0; i < (*arr).size() ; i++) {
-        (*arr)[i] += 1;
-    }
 }
 
 
