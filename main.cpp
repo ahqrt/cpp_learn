da1a8541df53e8f6b8272b757e585971e71e51e0
yangliye <1217223113@qq.com>
2023-03-29 18:56:17 +0800

    chore: add ref of struct

--- main.cpp
+++ main.cpp
@@ -1,25 +1,51 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <cmath>
 
 using namespace std;
-int sum_arr(int arr[], int n);
+
+struct Polar
+{
+    double distance;
+    double angle;
+};
+
+struct Rect
+{
+    double x;
+    double y;
+};
+
+void show_polar(const Polar *pda);
+void rect_to_polar(const Rect* pxy, Polar* pda);
 
 int main() {
-    const int ArSize = 8;
-    int cookies[ArSize] {1, 2, 4, 8, 16, 32, 64, 128};
-    int sum = sum_arr(cookies, ArSize);
-    cout << sum << endl;
+
+    Rect rplace;
+    Polar pplace;
+
+    cout << "Enter the x and y values";
+    while (cin>>rplace.x >> rplace.y)
+    {
+        rect_to_polar(&rplace, &pplace);
+        show_polar(&pplace);
+        cout << "Next two numbers (q to quit): ";
+    };
+
+    cout << "Done.n";
+
     return 0;
 }
 
-int sum_arr(int arr[], int n)
+void show_polar(const Polar *pda)
 {
-    int total = 0;
-    for (int i = 0; i < n; i++) {
-        total += arr[i];
-    }
-    return total;
+    const double Rad_to_deg = 57.29577951;
+
+    cout << "distance = " << pda->distance;
+    cout << ", angle =" <<pda->angle * Rad_to_deg << " degrees" << endl;
 }
 
-
+void rect_to_polar(const Rect* pxy, Polar* pda)
+{
+    pda->distance = std::sqrt(pxy->x * pxy->y + pxy->y * pxy->y);
+    pda->angle = std::atan2(pxy->y, pxy->x);
+}
