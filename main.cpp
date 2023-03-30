104f30aec669785a28c633d0573d9abee3fa6698
yangliye <1217223113@qq.com>
2023-03-30 19:44:39 +0800

    chore: 自定义错误

--- main.cpp
+++ main.cpp
@@ -1,94 +1,60 @@
-#include <string>
 #include <iostream>
-#include <memory>
-#include "Message.h"
-#include <functional>
-// 函数模版
-template <typename T, typename = std::enable_if_t<std::is_integral_v<T> || std::is_floating_point_v<T>>>
-T getLargestNumber(const vector<T>& vect)
-{
-    T largestNumber {vect.at(0)};
+#include <vector>
+using namespace std;
 
-    for (auto& val : vect) {
-        if (val > largestNumber) {
-            largestNumber = val;
-        }
-    }
-    return largestNumber;
-}
-
-template<typename T1, typename T2, typename = std::enable_if<std::is_integral_v<T1> && std::is_floating_point_v<T2>>>
-auto getBiggerNum(T1 param1, T2 param2)
-{
-    return param1 > param2 ? param1 : param2;
-}
-
-
-// 模版类 键值对
-template <typename T>
-class KeyValPair
+class CustomError: public exception
 {
 public:
-    string key;
-    T value;
-    KeyValPair(string k, T v): key(k), value(v) {};
+    CustomError(int code, const string& message):m_code(code), m_message(message) {}
+
+    const char* what() const noexcept override
+    {
+        return (m_message + "[Error code: " + to_string(m_code) + "]").c_str();
+    }
+
+    int code() const {return  m_code; }
+private:
+    string m_message;
+    int m_code;
 };
 
-// 模版类 键值字典
-template <typename T>
-class Dic
-{
-public:
-    vector<KeyValPair<T>> vect;
-    Dic() = default;
-    void pushBack(KeyValPair<T> val)
-    {
-        vect.push_back(val);
+//void print(string&& str)
+//{
+//    str = "hello liuliu";
+//    cout << str << endl;
+//}
+template<typename T>
+inline void print(T&& str) {
+    str = "Hello world";
+    cout << str << endl;
+}
+
+
+string getPhoneNum() {
+    cout << "请输入手机号码" << endl;
+    string result;
+    cin >> result;
+    if (result.length() != 11) {
+        throw CustomError(12, "手机号长度不够");
+    }
+    else{
+        return result;
+    };
+}
+
+
+int main() {
+
+//        左值是表达式结束后依然存在的持久对象（代表一个在内存中占有确定位置的对象），右值是表达式结束时不再存在的临时对象
+    string phoneNum;
+    try {
+        phoneNum = getPhoneNum();
+    } catch (const exception& ex) {
+        cout << ex.what() << endl;
+        return 0;
     };
 
-    auto operator[](string key)
-    {
-        for (KeyValPair<T>& pair: vect) {
-            if (pair.key == key) {
-                return pair.value;
-            }
-        }
-    }
-};
-
-
-//void print(string& content) {
-//    cout << content << endl;
-//}
-
-// 为函数指针定义别名
-using printType = void (*) (string&);
-
-// 最好使用标准库的function模版
-//void callPrint(printType f, string& param) {
-void callPrint(function<void(string&)> f, string& param) {
-    f(param);
-}
-auto print = [](auto param) {
-    std::cout << param << std::endl;
-};
-
-int main()
-{
-////    string content{"hello world"};
-////
-//////    callPrint(print, content);
-////    vector<int> vect{ 0,1,2,3,4,5,6,7,8,9 };
-//////    C++ 中的匿名函数通常以 [capture](parameters) -> returnType { body }
-////    for_each(begin(vect), end(vect), [](int param) {
-////        cout << param << endl;
-////    });
-//    string str { "等忙完这一阵子，" };
-//    string str2 {"就可以接着忙下一阵子了。"};
-//    auto print = [&str](string& param) {
-//        string str2 { "马克思说：" };
-//        //输出： 马克思说：等忙完这一阵子，就可以接着忙下一阵子了。
-//        cout << str2 << str << param << std::endl;
-//    };
-    print(123);
+    cout << "用户输入的手机号是" << phoneNum << endl;
+//    cout << "用户输入的手机号是" << e.code << endl;
+    return 0;
 }
