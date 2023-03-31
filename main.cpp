39bbaa7d2f78f9001d95c60f50aa5545951894e0
yangliye <1217223113@qq.com>
2023-03-31 14:35:57 +0800

    chore: 常用函数

--- main.cpp
+++ main.cpp
@@ -1,60 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <format>
+
 using namespace std;
 
-class CustomError: public exception
-{
-public:
-    CustomError(int code, const string& message):m_code(code), m_message(message) {}
-
-    const char* what() const noexcept override
-    {
-        return (m_message + "[Error code: " + to_string(m_code) + "]").c_str();
-    }
-
-    int code() const {return  m_code; }
-private:
-    string m_message;
-    int m_code;
+//移除字符串左侧空白符
+void ltrim(string& s) {
+    auto endPosition = find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !isspace(ch);
+    });
+    s.erase(s.begin(), endPosition);
 };
 
-//void print(string&& str)
-//{
-//    str = "hello liuliu";
-//    cout << str << endl;
-//}
-template<typename T>
-inline void print(T&& str) {
-    str = "Hello world";
-    cout << str << endl;
+
+void replace(function<void (string& a)> fn, string& s)
+{
+    fn(s);
 }
 
-
-string getPhoneNum() {
-    cout << "请输入手机号码" << endl;
-    string result;
-    cin >> result;
-    if (result.length() != 11) {
-        throw CustomError(12, "手机号长度不够");
-    }
-    else{
-        return result;
-    };
+void print(string& s) {
+    cout << s << endl;
 }
-
 
 int main() {
+    string string1 {"    liuliu"};
 
-//        左值是表达式结束后依然存在的持久对象（代表一个在内存中占有确定位置的对象），右值是表达式结束时不再存在的临时对象
-    string phoneNum;
-    try {
-        phoneNum = getPhoneNum();
-    } catch (const exception& ex) {
-        cout << ex.what() << endl;
-        return 0;
-    };
-
-    cout << "用户输入的手机号是" << phoneNum << endl;
-//    cout << "用户输入的手机号是" << e.code << endl;
-    return 0;
+    replace( [](string& s) {
+        cout << s << endl;
+    }, string1);
 }
