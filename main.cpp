d1d4fac9f4a989b9ed60aa0fb733ff75c90c8940
yangliye <1217223113@qq.com>
2023-03-31 17:11:15 +0800

    feat: 查看电脑支持开多少个线程

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-#include <thread>  //线程类型在此头文件中定义
+#include <thread>
 #include <string>
 using namespace std;
 
 int main() {
-    auto func = [](int tId, int num) {
-        for (std::size_t i = 0; i < num; i++) {
-            string str = "thread id" + to_string(tId) + " print: " + to_string(i) + "\n";
-            cout << str;
-        }
+    auto func = [](int i) {
+        string str = "第" + to_string(i) + "个线程在工作"+ "\n";
+        cout << str;
     };
-
-    int num = 6;
-
-    thread t(func, 0, std::ref(num));
-//    thread t2(func, 1, 6);
-    t.join();
-//    t2.join();
+    //我的实验设备CPU为i9-9900K，8核16线程
+    //此处得到的值为16
+    int threadNum = thread::hardware_concurrency();
+    threadNum = threadNum == 0 ? 2 : threadNum;  //确保线程数量不要为0
+    threadNum = min(threadNum, 32);  //确保线程数量不要超过32个
+    //输出：我的电脑可以开启16个线程
+    cout << "我的电脑可以开启" << threadNum << "个线程" << endl;
+    for (size_t i = 0; i < threadNum; ++i) {
+        thread t(func,i);
+        t.detach();
+    }
+    auto c = getchar();
 }
